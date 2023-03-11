05108481c924825f5468744f2dab8250210e7bf4
Reeyan Khimani <Khimanireeyan@gmail.com>
2023-03-11 15:43:24 +0000

    added ai

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a junior <br className='sm:block hidden' />
-            and love to learn about Computer Science!
+            and love to learn about Computer Science and !
           </p>
         </div>
       </div>
